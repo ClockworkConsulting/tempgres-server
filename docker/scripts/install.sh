4caa381a16d8d851d4e70b13c1d79ce00c50c1b0
Bardur Arantsson <ba@cwconsult.dk>
2016-03-11 08:52:09 +0100

    Tweak docker install script to support old base images

--- docker/scripts/install.sh
+++ docker/scripts/install.sh
@@ -2,14 +2,17 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Make sure we have an up-to-date base distribution
+apt-get update
+apt-get dist-upgrade -y
+
 # Add PostgreSQL APT repository
 apt-get install -y wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 
-# Make sure we have an up-to-date base distribution
+# Update package list
 apt-get update
-apt-get dist-upgrade -y
 
 # Install basic packages
 apt-get install -y \
