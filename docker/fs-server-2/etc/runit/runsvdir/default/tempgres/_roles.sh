34a7b061b778584d30c5b6b8ec2bf8554c747d42
Bardur Arantsson <bardur@scientician.net>
2021-05-06 20:44:43 +0200

    Fix role creation
    Role creation should NOT be conditional, or at least not depend on
    keeping state on the file system. We recreate the cluster from scratch
    every time we start, so doing so will mean that users aren't created
    properly on restart.

--- docker/fs-server-2/etc/runit/runsvdir/default/tempgres/_roles.sh
+++ docker/fs-server-2/etc/runit/runsvdir/default/tempgres/_roles.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-# Don't try to create roles if already created. This guards against
-# restarts of the main server.
-if [ -f $HOME/.roles-created ]; then
-  exit 0
-fi
-
 echo "Creating roles..."
 psql -o /dev/null <<EOF
 CREATE ROLE "${TEMPGRES_ADMIN_USER}" ENCRYPTED PASSWORD '${TEMPGRES_ADMIN_PASS}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
 CREATE ROLE "${TEMPGRES_CLIENT_USER}" ENCRYPTED PASSWORD '${TEMPGRES_CLIENT_PASS}' INHERIT LOGIN;
 EOF
-
-touch $HOME/.roles-created
