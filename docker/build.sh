65fb2db707c543470c2f811476bb3376405808b1
Bardur Arantsson <bardur@scientician.net>
2022-03-30 01:53:58 +0200

    Clean up directory handling in build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONTEXT_DIR="$(realpath ${SCRIPT_DIR}/..)"
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+CONTEXT_DIR="${ROOT_DIR}"
 
-DOCKER_BUILDKIT=1 docker build -f docker/Dockerfile -t tempgres:latest "${CONTEXT_DIR}"
+DOCKER_BUILDKIT=1 docker build -f "${CONTEXT_DIR}/docker/Dockerfile" -t tempgres:latest "${CONTEXT_DIR}"
